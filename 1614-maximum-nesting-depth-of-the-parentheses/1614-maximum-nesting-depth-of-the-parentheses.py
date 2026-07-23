4f2152288e74140a0fe883a41169b2dd33386e21
Shashank <157578332+shashank-project-github@users.noreply.github.com>
2026-07-23 11:40:19 +0530

    Time: 0 ms (100%), Space: 12.4 MB (17.57%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxDepth(self, s):
+        depth = 0
+        max_depth = 0
+
+        for ch in s:
+            if ch == '(':
+                depth += 1
+                max_depth = max(max_depth , depth)
+            elif ch == ')':
+                depth -= 1
+        
+        return max_depth
