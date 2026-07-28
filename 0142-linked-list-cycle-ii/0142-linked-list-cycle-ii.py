d7f1a3da85d725c53f12fc90e5f8cf9fddac6700
Shashank <157578332+shashank-project-github@users.noreply.github.com>
2026-07-28 23:34:47 +0530

    Time: 32 ms (60.57%), Space: 18.7 MB (55.77%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def detectCycle(self, head):  
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast= fast.next.next
+
+            if slow == fast:
+                break
+
+        else:
+            return None
+
+        slow = head
+
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+
+        return slow
